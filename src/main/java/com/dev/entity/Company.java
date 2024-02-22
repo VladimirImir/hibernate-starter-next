d58c1e87cb833341c48934afc022d1ea81838144
VladimirImir <mimimvvv@gmail.com>
2024-02-22 23:00:12 +0200

    Hibernate. 38. - ElementCollection.

--- src/main/java/com/dev/entity/Company.java
+++ src/main/java/com/dev/entity/Company.java
@@ -3,6 +3,7 @@ package com.dev.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -27,6 +28,13 @@ public class Company {
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
     //@JoinColumn(name = "company_id")
     private Set<User> users = new HashSet<>();
+
+    @Builder.Default
+    @ElementCollection
+    @CollectionTable(name = "company_locale", joinColumns = @JoinColumn(name = "company_id"))
+    @Column(name = "description")
+    private List<LocaleInfo> locales = new ArrayList<>();
+    //private List<String> locales = new ArrayList<>();
 
     public void addUser(User user) {
         users.add(user);
