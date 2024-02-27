121a5ac2b6b1aa96b0ee713e5fea7a2e7114f38a
VladimirImir <mimimvvv@gmail.com>
2024-02-27 06:44:15 +0200

    Hibernate. 46. - Inheritance. JOINED.

--- src/main/java/com/dev/entity/Programmer.java
+++ src/main/java/com/dev/entity/Programmer.java
@@ -5,18 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@DiscriminatorValue("programmer")
-public class Programmer extends User{
+@PrimaryKeyJoinColumn(name = "id")
+public class Programmer extends User {
 
     @Enumerated(EnumType.STRING)
     private Language language;
--- src/main/java/com/dev/entity/User.java
+++ src/main/java/com/dev/entity/User.java
@@ -19,8 +19,7 @@ import java.util.Set;
 @Entity
 @Table(name = "users", schema = "public")
 //@TypeDef(name = "dev", typeClass = JsonBinaryType.class)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User implements Comparable<User>, BaseEntity<Long> {
 
     /*@Id
