3a9d361545a4fef9976cbac90c56bcb48a0b5bfe
VladimirImir <mimimvvv@gmail.com>
2024-03-12 17:17:25 +0200

    Hibernate. 56. - @Fetch.

--- src/main/java/com/dev/entity/User.java
+++ src/main/java/com/dev/entity/User.java
@@ -7,9 +7,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.*;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.CascadeType;
@@ -81,7 +79,8 @@ public class User implements Comparable<User>, BaseEntity<Long> {
     private Set<UserChat> userChats = new HashSet<>();
 
     @Builder.Default
-    @BatchSize(size = 3)
+    //@BatchSize(size = 3)
+    @Fetch(FetchMode.SUBSELECT)
     @OneToMany(mappedBy = "receiver")
     private List<Payment> payments = new ArrayList<>();
 
