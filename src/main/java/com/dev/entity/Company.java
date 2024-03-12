11e1fdcb1a7bbd5cf6cca99235ceb8110fc4a057
VladimirImir <mimimvvv@gmail.com>
2024-03-12 17:04:32 +0200

    Hibernate. 55. - @BatchSize.

--- src/main/java/com/dev/entity/Company.java
+++ src/main/java/com/dev/entity/Company.java
@@ -1,6 +1,7 @@
 package com.dev.entity;
 
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.SortNatural;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.*;
 @ToString(exclude = "users")
 @Builder
 @Entity
+@BatchSize(size = 3)
 public class Company {
 
     @Id
