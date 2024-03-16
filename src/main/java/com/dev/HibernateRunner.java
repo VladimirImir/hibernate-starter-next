cc938b48ce513995ad8c2c4dfcbc190f3e310d8d
VladimirImir <mimimvvv@gmail.com>
2024-03-16 23:12:37 +0200

    Hibernate. 74. - Hibernate Envers. Part 3.

--- src/main/java/com/dev/HibernateRunner.java
+++ src/main/java/com/dev/HibernateRunner.java
@@ -7,11 +7,11 @@ import com.dev.util.HibernateUtil;
 import com.dev.util.TestDataImporter;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
 import org.hibernate.graph.GraphSemantic;
 import org.hibernate.graph.RootGraph;
 import org.hibernate.graph.SubGraph;
@@ -24,6 +24,7 @@ import javax.transaction.Transactional;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -33,18 +34,34 @@ public class HibernateRunner {
     @Transactional
     public static void main(String[] args) throws SQLException {
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-             Session session = sessionFactory
-                     .withOptions()
-                     .interceptor(new GlobalInterceptor())
-                     .openSession()) {
-            TestDataImporter.importData(sessionFactory);
-            session.beginTransaction();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+//            TestDataImporter.importData(sessionFactory);
+            try (var session = sessionFactory.openSession()) {
+                session.beginTransaction();
 
-            var payment = session.find(Payment.class, 1L);
-            payment.setAmount(payment.getAmount() + 10);
+                var payment = session.find(Payment.class, 1L);
+                payment.setAmount(payment.getAmount() + 10);
 
-            session.getTransaction().commit();
+                session.getTransaction().commit();
+            }
+            try (var session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
+
+                var auditReader = AuditReaderFactory.get(session2);
+//                auditReader.find(Payment.class, 1L, 1L);
+                var oldPayment = auditReader.find(Payment.class, 1L, new Date(1710532249006L));
+                session2.replicate(oldPayment, ReplicationMode.OVERWRITE);
+
+                auditReader.createQuery()
+                        .forEntitiesAtRevision(Payment.class, 400L)
+                        .add(AuditEntity.property("amount").ge(450))
+                        .add(AuditEntity.property("id").ge(6L))
+                        .addProjection(AuditEntity.property("amount"))
+                        .addProjection(AuditEntity.id())
+                        .getResultList();
+
+                session2.getTransaction().commit();
+            }
         }
     }
 }
